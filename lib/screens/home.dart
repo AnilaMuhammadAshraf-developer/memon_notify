b14bc4929dae3a03a0d5655a5974921c21c80b24
AnilaMuhammadAshraf-developer <anilamuhammadashraf.software@gmail.com>
2025-06-06 17:02:01 +0500

    push notifcation done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,21 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:memon_notify/utils/app_strings.dart';
 import 'package:memon_notify/widgets/custome_app_bar.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   final fcmToken;
-  const HomeScreen({super.key,required this.fcmToken});
+  const HomeScreen({super.key, required this.fcmToken});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title:AppStrings.appTitle),
-      body:Center(
-        child: SelectableText(
-            fcmToken != null
-                ? 'Token:\n$fcmToken'
-                : 'Getting FCM token...',
-            textAlign: TextAlign.center,
+      appBar: CustomAppBar(title: AppStrings.appTitle),
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Text(
+                    "Your Device Token",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  fcmToken != null
+                      ? SelectableText('$fcmToken', textAlign: TextAlign.left)
+                      : Text('Getting FCM token...'),
+                ],
+              ),
+            ),
           ),
-      )
+        ],
+      ),
     );
   }
 }
